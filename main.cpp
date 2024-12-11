f2880a4edb4a39d27aaf948be05ca2214812a03e
Casey Huynh <85851187+caseyhuynh@users.noreply.github.com>
2024-12-10 18:24:30 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Particles.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "Engine.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	// Declare an instance of Engine
+	Engine engine;
+	// Start the engine
+	engine.run();
+	// Quit in the usual way when the engine is stopped
+	return 0;
+}
+
